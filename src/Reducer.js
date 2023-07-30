ca41b2836c3fa5e4d6ff89e3172dfc6a148b838b
rizul11 <rizulsharma.offical@gmail.com>
2023-07-30 17:10:17 +0530

    commit

---
+++ src/Reducer.js
@@ -0,0 +1,20 @@
+import React, { useReducer } from "react";
+
+function Reducer() {
+  const initialState = 0;
+  const reducer = (state, action) => {
+    return;
+  };
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <h1>Count = {count}</h1>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("Decrement")}>Decrement</button>
+      <button onClick={() => dispatch("Reset")}>Reset</button>
+    </>
+  );
+}
+
+export default Reducer;
