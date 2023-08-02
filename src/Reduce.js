545ae1a533b3a77015e0cd8729b83f9b513da289
rizul11 <rizulsharma.offical@gmail.com>
2023-08-02 15:16:04 +0530

    commit

--- src/Reduce.js
+++ src/Reduce.js
@@ -1,5 +1,24 @@
-import React from 'react'
+import {useReducer} from 'react'
 
+function reducer(state, action){
+    switch(action.type){
+        case 'incremented_age': {
+            return {
+                name: state.name,
+                age:state.age + 1
+            };
+        }
+       case 'chnaged_name': {
+        return {
+            name:action.nextName,
+            age:state.age
+        };
+       }
+    }
+    throw Error('Unknown action' + action.type);
+}
+
+const initialState = {}
 function Reduce() {
   return (
     <div>
