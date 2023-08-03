606a3041066de791cb2bb007d672deb4714f4cf6
rizul11 <rizulsharma.offical@gmail.com>
2023-08-03 13:36:04 +0530

    Minor Changes

--- src/FetchAPI.js
+++ src/FetchAPI.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function FetchAPI() {
     fetch("http://ergast.com/api/f1/2019")
+    .then((response) => {
+       console.log(response);
+    })
   return (
     <div>
-      
+      F1
     </div>
   )
 }
