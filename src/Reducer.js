f35c33f8bff2253bf08415344e155007ec184d31
rizul11 <rizulsharma.offical@gmail.com>
2023-08-08 17:22:16 +0530

    Minor Changes

--- src/Reducer.js
+++ src/Reducer.js
@@ -4,14 +4,14 @@ function Reducer() {
   const initialState = 0;
   const reducer = (state, action) => {
     switch (action) {
-        case 'increment':
-            return state + 1 
-        case 'decrement':
-            return state - 1
-            case 'reset' :
-                return initialState
-                default:
-                    return state
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      case "reset":
+        return initialState;
+      default:
+        return state;
     }
   };
   const [count, dispatch] = useReducer(reducer, initialState);
