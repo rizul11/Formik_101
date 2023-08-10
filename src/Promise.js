bbbd06c0c22f4ba8fe8b728db9cb544d1c230537
rizul11 <rizulsharma.offical@gmail.com>
2023-08-10 13:54:54 +0530

    commit

--- src/Promise.js
+++ src/Promise.js
@@ -4,6 +4,7 @@ const THRESHOLD_A = 8;
 
 function tetheredGetNumber(resolve, reject) {
     setTimeout(() => {
-        
+    const randomInt = Date.now();
+    const value = randomInt % 10;  
     })
 }
