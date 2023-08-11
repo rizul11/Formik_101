996576118fab5a3572d98111f1989bc3eb302d01
rizul11 <rizulsharma.offical@gmail.com>
2023-08-11 18:56:18 +0530

    commit

--- src/FormikForm.js
+++ src/FormikForm.js
@@ -1,16 +1,41 @@
-import { Form, Formik } from 'formik'
-import React from 'react'
+import { Field, Form, Formik } from "formik";
+import React from "react";
 
 function FormikForm() {
   return (
     <div>
-      <Formik>
+      <Formik
+        initialValues={{ name: "", age: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
         <Form>
-            
+          <label htmlFor="">Enter Name :</label>
+          <Field type="text" name="name" />
+          <br />
+          <br />
+          <label htmlFor="">Enter Age :</label>
+          <Field type="number" name="age" />
+          <br />
+          <br />
+          <label htmlFor="">Enter Password</label>
+          <Field type="password" name="password" />
+          <br />
+          <br />
+          <label htmlFor="">Gender: &nbsp;</label>
+          <label htmlFor="">Male &nbsp;</label>
+          <Field type="radio" name="gender" value="Male" />
+          &nbsp;
+          <label htmlFor="">Female &nbsp;</label>
+          <Field type="radio" name="gender" value="Female" />
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
         </Form>
       </Formik>
     </div>
-  )
+  );
 }
 
-export default FormikForm
+export default FormikForm;
