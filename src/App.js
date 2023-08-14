b16a773decd2ff3c4e3f19e4e7697fe133fb5688
rizul11 <rizulsharma.offical@gmail.com>
2023-08-14 17:47:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import { useFormik } from "formik";
 import React from "react";
 import { FormSchema } from "./FormSchema";
 
-
 function App() {
-    
   const formInitialValues = {
-    name:'',
-    email:''
-  }
-  const {handleSubmit , handleChange , values, errors} = useFormik({
-       initialValues: formInitialValues,
-       validationSchema: FormSchema,
-       onSubmit:(values) => {
+    name: "",
+    email: "",
+    age: "",
+    password: "",
+  };
+  const { handleSubmit, touched, handleBlur, handleChange, values, errors } =
+    useFormik({
+      initialValues: formInitialValues,
+      validationSchema: FormSchema,
+      onSubmit: (values, action ) => {
         console.log(values);
         console.log(values.name);
         console.log(values.email);
-       }
-    })
+        console.log(values.age);
+        console.log(values.password);
+        action.resetForm();
+      },
+    });
   return (
     <div>
       <div>
@@ -27,31 +31,71 @@ function App() {
           <input
             type="text"
             name="name"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={values.name}
             required
           />
           <br />
-          <span style={{color:'red'}}>{errors.name}</span>
+
+          {errors.name && touched.name ? (
+            <span style={{ color: "red" }}>{errors.name}</span>
+          ) : null}
           <br />
           <br />
           <label htmlFor="">Enter your Email:</label>
           <input
             type="text"
             name="email"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={values.email}
             required
           />
           <br />
-          <span style={{color:'red'}}>{errors.email}</span>
+          {errors.email && touched.email ? (
+            <span style={{ color: "red" }}>{errors.email}</span>
+          ) : null}
+          <br />
+          <br />
+
+          <label htmlFor="">Enter Age:</label>
+          <input
+            type="text"
+            name="age"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.age}
+            required
+          />
+          <br />
+
+          {errors.age && touched.age ? (
+            <span style={{ color: "red" }}>{errors.age}</span>
+          ) : null}
+          <br />
+          <br />
+          <label htmlFor="">Enter Password:</label>
+          <input
+            type="text"
+            name="password"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.password}
+            required
+          />
+          <br />
+
+          {errors.password && touched.password ? (
+            <span style={{ color: "red" }}>{errors.password}</span>
+          ) : null}
+          <br />
+          <br />
           <input type="submit" value="Submit" />
         </form>
       </div>
     </div>
   );
-};
-
+}
 
 export default App;
-
--- src/FormSchema.js
+++ src/FormSchema.js
@@ -3,4 +3,10 @@ import * as Yup from "yup";
 export const FormSchema = Yup.object({
   name: Yup.string().min(3, 'Too Short').max(20, 'Too Long').required("Name is must"),
   email: Yup.string().email().required("Email is Must"),
+  age:Yup.number().min(10).max(50).required("Age is must"),
+//   password:Yup.string().required("Password is must").matches(
+//     "^(?=.*[A-Za-z])(?=.*d){8,}$",
+//     "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+//   ),
+
 });
