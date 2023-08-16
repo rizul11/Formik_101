ea1cba6f675b60da1a8475edf18e1f9132f6fb74
rizul11 <rizulsharma.offical@gmail.com>
2023-08-16 17:54:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { Formik , Form, Field , ErrorMessage } from 'formik'; 
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function App() {
-  return(
-    <div className='App'>
+  return (
+    <div className="App">
       <center>
         <h1>Register a new account</h1>
         <Formik>
-          
+          {({ isSubmitting }) => (
+            <Form>
+              <Field
+                type="text"
+                name="fullname"
+                placeholder="Enter your fullname"
+              />
+              <ErrorMessage name="fullname" component="div" />
+              <Field
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+              />
+              <ErrorMessage name="email" component="div" />
+              <Field
+                type="password"
+                name="password"
+                placeholder="password"
+                component="div"
+              />
+              <ErrorMessage name="password" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
         </Formik>
       </center>
-
     </div>
   );
 }
+
+export default App;
