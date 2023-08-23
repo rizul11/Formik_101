d45c8f6b4a1bcca7ab7849999de0200c5b2d079d
rizul11 <rizulsharma.offical@gmail.com>
2023-08-23 11:45:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+// import React from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <center>
+//         <h1>Register a new account</h1>
+//         <Formik>
+//           {({ isSubmitting }) => (
+//             <Form>
+//               <Field
+//                 type="text"
+//                 name="fullname"
+//                 placeholder="Enter your fullname"
+//               />
+//               <ErrorMessage name="fullname" component="div" />
+//               <Field
+//                 type="email"
+//                 name="email"
+//                 placeholder="Enter your email address"
+//               />
+//               <ErrorMessage name="email" component="div" />
+//               <Field
+//                 type="password"
+//                 name="password"
+//                 placeholder="password"
+//                 component="div"
+//               />
+//               <ErrorMessage name="password" component="div" />
+//               <button type="submit" disabled={isSubmitting}>
+//                 Submit
+//               </button>
+//             </Form>
+//           )}
+//         </Formik>
+//       </center>
+//     </div>
+//   );
+// }
+
+// export default App; 
+
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <center>
-        <h1>Register a new account</h1>
-        <Formik>
-          {({ isSubmitting }) => (
-            <Form>
-              <Field
-                type="text"
-                name="fullname"
-                placeholder="Enter your fullname"
-              />
-              <ErrorMessage name="fullname" component="div" />
-              <Field
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-              />
-              <ErrorMessage name="email" component="div" />
-              <Field
-                type="password"
-                name="password"
-                placeholder="password"
-                component="div"
-              />
-              <ErrorMessage name="password" component="div" />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </center>
+    <div>
+      <div>
+        <h1>Form validation</h1>
+        <form>
+          <label htmlFor="">Enter Name:</label>
+          <input type='text' name=''/>
+          <br /><br />
+          <input type='submit' value="Submit" />
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
