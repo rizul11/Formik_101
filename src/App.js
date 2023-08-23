6a2a007a7013e4641c13e9a8e515504bab91a6bf
rizul11 <rizulsharma.offical@gmail.com>
2023-08-23 13:00:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
   const [email, setEmail] =useState();
   const [nameError, setNameError] = useState(false);
   const [EmailError, setEmailError] = useState(false);
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
   const handleName = (e) => {
      let name = e.target.value;
      if(name.length < 3){
@@ -61,6 +62,13 @@ function App() {
   }
   const handleEmail= (e) => {
     let email = e.target.value;
+    if(!email.match(emailRegex))
+    {
+      setEmailError(true);
+    }
+    else{
+      setEmailError(false);
+    }
   }
 return (
     <div>
@@ -73,7 +81,8 @@ return (
             </span> : ""}
           <br /><br />
           <label htmlFor=''>Enter your Email:</label>
-          <input type='text' name='email' onChange={handleEmail} value={email} required />
+          <input type='text' name='email' onChange={handleEmail} value={email} required /><br/>
+          {EmailError ? <span style={{color:'red'}}>Name should be more than 2 letters  </span> : ""}
           <input type='submit' value="Submit" />
         </form>
       </div>
