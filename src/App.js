21775b3b6d89f7be76ae1b9049af5c6690db9dcb
rizul11 <rizulsharma.offical@gmail.com>
2023-08-24 13:56:55 +0530

    .

--- src/App.js
+++ src/App.js
@@ -104,7 +104,15 @@ const SignupForm = () => {
     },
     onSubmit: values => {
       alert(JSON.stringify(values,null,2));
-    }
-  })
-}
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+    <label htmlFor="email">Email Address</label>
+    <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+    <button type="submit">Submit</button>
+    </form>
+
+  );
+};
 
