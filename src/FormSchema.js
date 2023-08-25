207a3bda2abb9a2ee82d8523cd7829f8aceec99c
rizul11 <rizulsharma.offical@gmail.com>
2023-08-25 16:00:33 +0530

    .

--- src/FormSchema.js
+++ src/FormSchema.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
 export const FormSchema = Yup.object({
-  name: Yup.string().min(3).max(20).required("Name is must"),
+  name: Yup.string().min(3, 'Too Short').max(20, 'Too Long').required("Name is must"),
   email: Yup.string().email().required("Email is Must"),
 });
